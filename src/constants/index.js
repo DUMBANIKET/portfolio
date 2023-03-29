80d1036f9b0b07a83f7c3ced4122f71ab87a8f41
Aniket Sen <aniket.singh21@vsit.edu.in>
2023-03-29 16:12:18 +0000

    changes

---
+++ src/constants/index.js
@@ -0,0 +1,239 @@
+import {
+  mobile,
+  backend,
+  creator,
+  web,
+  javascript,
+  typescript,
+  html,
+  css,
+  reactjs,
+  redux,
+  tailwind,
+  nodejs,
+  mongodb,
+  git,
+  figma,
+  docker,
+  meta,
+  starbucks,
+  tesla,
+  shopify,
+  carrent,
+  jobit,
+  tripguide,
+  threejs,
+} from "../assets";
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [
+  {
+    title: "Programmer",
+    icon: web,
+  },
+  {
+    title: "Android Developer",
+    icon: mobile,
+  },
+  {
+    title: "Full Stack Developer",
+    icon: backend,
+  },
+  {
+    title: "Content Creator",
+    icon: creator,
+  },
+ 
+];
+
+const technologies = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "TypeScript",
+    icon: typescript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Tailwind CSS",
+    icon: tailwind,
+  },
+  {
+    name: "Node JS",
+    icon: nodejs,
+  },
+  {
+    name: "MongoDB",
+    icon: mongodb,
+  },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
+  {
+    name: "git",
+    icon: git,
+  },
+  {
+    name: "figma",
+    icon: figma,
+  },
+  {
+    name: "docker",
+    icon: docker,
+  },
+];
+
+const experiences = [
+  {
+    title: "Flutter Front End Developer",
+    company_name: "WeekendStory",
+    icon: "https://yt3.ggpht.com/-7zFDHK5X45w/AAAAAAAAAAI/AAAAAAAAAAA/QJfHeLTEZwE/s900-c-k-no-mo-rj-c0xffffff/photo.jpg",
+    iconBg: "#383E56",
+    date: "August 2022 - October 2022",
+    points: [
+      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      "Implementing responsive design and ensuring cross-browser compatibility.",
+      "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+  },
+  {
+    title: "Graphic Designer",
+    company_name: "ElightLabs",
+    icon: "https://yt3.ggpht.com/-7zFDHK5X45w/AAAAAAAAAAI/AAAAAAAAAAA/QJfHeLTEZwE/s900-c-k-no-mo-rj-c0xffffff/photo.jpg",
+    iconBg: "#E6DEDD",
+    date: "iska idea nahi",
+    points: [
+      "graphics Designer wale cheeje ",
+    ],
+  },
+  
+  
+];
+
+const testimonials = [
+  {
+    testimonial:
+      "hmmm m mmm m m m m m m m m m m m m m mm.",
+    name: "Abhishek",
+    designation: "RTX",
+    company: "IDk",
+    image: "https://th.bing.com/th/id/OIP.e-cvpM1sPulBIEzbZMHY9wHaFQ?pid=ImgDet&rs=1",
+  },
+  {
+    testimonial:
+      "Burg.",
+    name: "Amit",
+    designation: "CEO",
+    company: "India",
+    image: "https://th.bing.com/th/id/OIP.e-cvpM1sPulBIEzbZMHY9wHaFQ?pid=ImgDet&rs=1",
+  },
+  {
+    testimonial:
+      "After Aniket optimized our website, our traffic increased by 50%. We can't thank them enough!",
+    name: "Sajid",
+    designation: "Pro Anime Website Creator",
+    company: "Adani Power",
+    image: "https://th.bing.com/th/id/OIP.e-cvpM1sPulBIEzbZMHY9wHaFQ?pid=ImgDet&rs=1",
+  },
+];
+
+const projects = [
+  {
+    name: "IDK",
+    description:
+      "yeh badd me likh duga",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "mongodb",
+        color: "green-text-gradient",
+      },
+      {
+        name: "tailwind",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: "https://th.bing.com/th/id/OIP.EMHLwRlGYdd2PCwYam-zxwHaEj?pid=ImgDet&rs=1",
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "IDK",
+    description:
+      "yeh badd me likh duga",
+    tags: [
+      {
+        name: "react",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "restapi",
+        color: "green-text-gradient",
+      },
+      {
+        name: "scss",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: "https://images.alphacoders.com/587/587355.jpg",
+    source_code_link: "https://github.com/",
+  },
+  {
+    name: "IDK",
+    description:
+      "yeh badd me likh duga",
+    tags: [
+      {
+        name: "nextjs",
+        color: "blue-text-gradient",
+      },
+      {
+        name: "supabase",
+        color: "green-text-gradient",
+      },
+      {
+        name: "css",
+        color: "pink-text-gradient",
+      },
+    ],
+    image: "https://wallpapercave.com/wp/wp4180312.jpg",
+    source_code_link: "https://github.com/",
+  },
+];
+
+export { services, technologies, experiences, testimonials, projects };
